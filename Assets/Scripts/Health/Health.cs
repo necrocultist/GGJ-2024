af78ac02c15885120e3916790217ce38adb00932
karatask20 <karataskn20@gmail.com>
2024-01-27 00:53:49 +0300

    Health scripts init

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthBar;
+    private int startingHealth;
+    private int currentHealth;
+    private HealthEvent healthEvent;
+    private Player player;
+    private bool isImmuneAfterHit = false;
+    private float immunityTime;
+    private Coroutine immunityCoroutine;
+    private SpriteRenderer spriteRenderer = null;
+    private const float spriteFlashInterval = 0.2f;
+    private WaitForSeconds WaitForSecondsSpriteFlashInterval = new WaitForSeconds(spriteFlashInterval);
+
+    [HideInInspector] public bool isDamageable = true;
+    [HideInInspector] public Enemy enemy;
+
+    private void Awake()
+    {
+        healthEvent = GetComponent<HealthEvent>();
+    }
+
+    private void Start()
+    {
+        CallHealthEvent(0);
+
+        player = GetComponent<Player>();
+        enemy = GetComponent<Enemy>();
+
+        if (player != null)
+        {
+            if (player.playerDetails.isImmuneAfterHit)
+            {
+                isImmuneAfterHit = true;
+                immunityTime = player.playerDetails.hitImmunityTime;
+                spriteRenderer = player.spriteRenderer;
+            }
+        }
+        else if (enemy != null)
+        {
+            if (enemy.enemyDetails.isImmuneAfterHit)
+            {
+                isImmuneAfterHit = true;
+                immunityTime = enemy.enemyDetails.hitImmunityTime;
+                spriteRenderer = enemy.spriteRenderer;
+            }
+        }
+
+        if (enemy != null && enemy.enemyDetails.isHealthBarDisplayed == true && healthBar != null)
+        {
+            healthBar.EnableHealthBar();
+        }
+        else if (healthBar != null)
+        {
+            healthBar.DisableHealthBar();
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (!isDamageable)
+        {
+            return;
+        }
+        
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+        }
+        
+        CallHealthEvent(damageAmount);
+        PostHitImmunity();
+        
+        if (healthBar != null)
+        {
+            healthBar.SetHealthBarValue((float)currentHealth / (float)startingHealth);
+        }
+    }
+
+    private void PostHitImmunity()
+    {
+        if (gameObject.activeSelf == false)
+            return;
+
+        if (isImmuneAfterHit)
+        {
+            if (immunityCoroutine != null)
+                StopCoroutine(immunityCoroutine);
+
+            immunityCoroutine = StartCoroutine(PostHitImmunityRoutine(immunityTime, spriteRenderer));
+        }
+    }
+    
+    private IEnumerator PostHitImmunityRoutine(float immunityTime, SpriteRenderer spriteRenderer)
+    {
+        int iterations = Mathf.RoundToInt(immunityTime / spriteFlashInterval / 2f);
+
+        isDamageable = false;
+
+        while (iterations > 0)
+        {
+            spriteRenderer.color = Color.red;
+
+            yield return WaitForSecondsSpriteFlashInterval;
+
+            spriteRenderer.color = Color.white;
+
+            yield return WaitForSecondsSpriteFlashInterval;
+
+            iterations--;
+
+            yield return null;
+
+        }
+
+        isDamageable = true;
+
+    }
+    private void CallHealthEvent(int damageAmount)
+    {
+        healthEvent.CallHealthChangedEvent(((float)currentHealth / (float)startingHealth), currentHealth, damageAmount);
+    }
+    
+    public void SetStartingHealth(int startingHealth)
+    {
+        this.startingHealth = startingHealth;
+        currentHealth = startingHealth;
+    }
+    
+    public int GetStartingHealth()
+    {
+        return startingHealth;
+    }
+    
+    public void AddHealth(int healthPercent)
+    {
+        int healthIncrease = Mathf.RoundToInt((startingHealth * healthPercent) / 100f);
+
+        int totalHealth = currentHealth + healthIncrease;
+
+        if (totalHealth > startingHealth)
+        {
+            currentHealth = startingHealth;
+        }
+        else
+        {
+            currentHealth = totalHealth;
+        }
+
+        CallHealthEvent(0);
+    }
+}
