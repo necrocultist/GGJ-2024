22efedbfa59c9ea3bf115d3fd075f7a1a2b681a3
Alper Oğuz Kelleci <kellecia20@itu.edu.tr>
2024-01-27 03:50:22 +0300

    room gen oldu

--- Assets/Scripts/DunGen/Room.cs
+++ Assets/Scripts/DunGen/Room.cs
@@ -1,36 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+namespace DunGen
 {
-    [SerializeField]
-    public int width;
-    public int height;
-    public int X;
-    public int Y;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Room : MonoBehaviour
     {
-        if(RoomControl.instance == null)
+        [SerializeField]
+        public int width;
+        public int height;
+        public int x;
+        public int y;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            Debug.Log("Start at wrong room");
+            if(RoomControl.instance == null)
+            {
+                Debug.Log("Start at wrong room");
+            }
+            
+            RoomControl.instance.RegisterRoom(this);
         }
-    }
 
-    void OnDrawGizmos(){
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
-    }
+        void OnDrawGizmos(){
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
+        }
 
-    public Vector3 GetRoomCenter(){
-        return new Vector3(X*width,Y*height);
-    }
+        public Vector3 GetRoomCenter(){
+            return new Vector3(x*width,y*height);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
     }
 }
--- Assets/Scripts/DunGen/RoomControl.cs
+++ Assets/Scripts/DunGen/RoomControl.cs
@@ -3,38 +3,106 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RoomInfo{
-    public string name;
-    public int X;
-    public int Y;
-}
-
-
-public class RoomControl : MonoBehaviour
+namespace DunGen
 {
-
-    public static RoomControl instance;
-    string currentWorldName = "First Floor";
-    RoomInfo currentLoadRoomData;
-    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
-    public List<Room> loadedRooms = new List<Room>();
-    bool isLoadingRoom = false;
-    void Awake(){
-        instance = this;
-    }
-    public bool DoesRoomExist(int x, int y){
-        return loadedRooms.Find(item=> item.X == x && item.Y == y)!= null;
+    public class RoomInfo{
+        public string name;
+        public int x;
+        public int y;
     }
 
-    // Start is called before the first frame update
-    void Start()
+
+    public class RoomControl : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        public static RoomControl instance;
+        string currentWorldName = "FirstFloor";
+        RoomInfo currentLoadRoomData;
+        Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+        public List<Room> loadedRooms = new List<Room>();
+        bool isLoadingRoom = false;
+        void Awake(){
+            instance = this;
+        }
+        public bool DoesRoomExist(int x, int y){
+            return loadedRooms.Find(item=> item.x == x && item.y == y)!= null;
+        }
+
+        public void LoadRoom(string name, int x, int y)
+        {
+            Debug.Log("ana");
+            if(DoesRoomExist(x,y))
+            {
+                return;
+            }
+            RoomInfo newRoomData = new RoomInfo();
+            newRoomData.name = name;
+            newRoomData.x = x;
+            newRoomData.y = y;
         
+            loadRoomQueue.Enqueue(newRoomData);
+        }
+
+        IEnumerator LoadRoomRoutine(RoomInfo info)
+        {
+            string roomName = currentWorldName + info.name;
+            AsyncOperation loadRoom = SceneManager.LoadSceneAsync(roomName, LoadSceneMode.Additive);
+            while (loadRoom.isDone == false)
+            {
+                yield return null;
+            }
+        }
+
+        public void RegisterRoom(Room room)
+        {
+            room.transform.position = new Vector3
+            (
+                currentLoadRoomData.x * room.width,
+                currentLoadRoomData.y * room.height,
+                0
+            );
+            room.x = currentLoadRoomData.x;
+            room.y = currentLoadRoomData.y;
+            room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.x + ", " + room.y;
+            room.transform.parent = transform;
+            isLoadingRoom = false;
+            loadedRooms.Add(room);
+        }
+
+        void UpdateRoomQueue()
+        {
+            if (isLoadingRoom)
+            {
+                return;
+            }
+
+            if (loadRoomQueue.Count == 0)
+            {
+                return;
+            }
+
+            currentLoadRoomData = loadRoomQueue.Dequeue();
+            isLoadingRoom = true;
+            StartCoroutine(LoadRoomRoutine(currentLoadRoomData));
+        }
+    
+    
+
+        void Start()
+        {       
+            LoadRoom("Start",0,0);
+            LoadRoom("Empty",1,0);
+            LoadRoom("Empty",2,0);
+            LoadRoom("Empty",1,1);
+
+        }
+    
+    
+
+
+        void Update()
+        {
+            UpdateRoomQueue();
+        }
     }
 }
